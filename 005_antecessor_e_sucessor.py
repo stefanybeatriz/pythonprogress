a63d2626b61ace51c1510d1d67b5f4a1a87f7f14
Stefany Beatriz <67984916+stefanybeatriz@users.noreply.github.com>
2024-03-04 18:27:59 -0300

    Add files via upload

---
+++ 005_antecessor_e_sucessor.py
@@ -0,0 +1,5 @@
+# Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor
+
+valor = int(input('Digite um valor: '))
+
+print(f'O antecessor de {valor} equivale a {valor - 1} e seu sucessor a {valor + 1}!')
