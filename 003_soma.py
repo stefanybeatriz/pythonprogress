eaa1c8c8145ef0d640b3513ec807367b2a74355a
Stefany Beatriz <67984916+stefanybeatriz@users.noreply.github.com>
2024-03-04 18:26:40 -0300

    Add files via upload

---
+++ 003_soma.py
@@ -0,0 +1,12 @@
+# Crie um programa que leia dois números e mostre a soma entre eles
+
+primeiro_valor = int(input('Digite o primeiro valor: '))
+segundo_valor = int(input('Digite o segundo valor: '))
+
+soma = primeiro_valor + segundo_valor
+
+print(f'A soma entre {primeiro_valor} e {segundo_valor} equivale a {soma}')
+
+'''Lembretes:
+Toda vez que desejarmos números como base de cálculo, devemos especificar seu tipo primitivo
+- numéro inteiro (no caso deste exercício) = int'''
