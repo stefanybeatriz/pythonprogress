4947ae838c8b58430e7dacac48cb8fa161f70ea8
Stefany Beatriz <67984916+stefanybeatriz@users.noreply.github.com>
2024-03-04 22:21:11 -0300

    Add files via upload

---
+++ 008_conversor_de_medidas.py
@@ -0,0 +1,5 @@
+# Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros
+
+medida = float(input('Digite o valor em metros que deseja converter: '))
+
+print(f'{medida} metros tem: \n- {medida * 100} centímetros \n- {medida * 1000} milímetros')
