d7d67e71c2df8e8d122603ab28181036bc4cc7b1
Stefany Beatriz <67984916+stefanybeatriz@users.noreply.github.com>
2024-03-04 22:20:10 -0300

    Add files via upload

---
+++ 006_multiplicacao.py
@@ -0,0 +1,5 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada
+
+valor = int(input('Digite um valor: '))
+
+print(f'Dobro: {valor * 2} \nTriplo: {valor * 3} \nRaiz quadrada: {valor ** 0.5}')
