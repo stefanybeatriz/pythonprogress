6706f6e7094e0590e6ec8ccecb7f70d0a4d9e8cb
Stefany Beatriz <67984916+stefanybeatriz@users.noreply.github.com>
2024-03-05 15:46:06 -0300

    Add files via upload

---
+++ 014_conversor_de_temperatura.py
@@ -0,0 +1,6 @@
+# Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit
+
+celsius = float(input('Digite uma temeperatura em graus ºC: '))
+fahrenheit = (celsius * 1.8) + 32
+
+print(f'{celsius} graus Celsius equivale a {fahrenheit} graus Fahrenheit')
