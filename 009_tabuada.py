1d21a00e1d0418a7a43a23a187ecc8606bf9d631
Stefany Beatriz <67984916+stefanybeatriz@users.noreply.github.com>
2024-03-04 22:21:39 -0300

    Add files via upload

---
+++ 009_tabuada.py
@@ -0,0 +1,15 @@
+# Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada
+
+num = int(input('Digite o número do qual deseja saber a tabuada: '))
+
+print(f'A tabuada do número {num} é: ')
+print(f'1 x {num} = {1 * num}')
+print(f'2 x {num} = {2 * num}')
+print(f'3 x {num} = {3 * num}')
+print(f'4 x {num} = {4 * num}')
+print(f'5 x {num} = {5 * num}')
+print(f'6 x {num} = {6 * num}')
+print(f'7 x {num} = {7 * num}')
+print(f'8 x {num} = {8 * num}')
+print(f'9 x {num} = {9 * num}')
+print(f'10 x {num} = {10 * num}')
