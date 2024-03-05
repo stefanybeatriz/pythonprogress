50d770bca37353ca1f9b4b2e2a326ea9cb001ea4
Stefany Beatriz <67984916+stefanybeatriz@users.noreply.github.com>
2024-03-04 22:20:29 -0300

    Add files via upload

---
+++ 007_media_aritmetica.py
@@ -0,0 +1,8 @@
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+
+media = float((nota1 + nota2) / 2)
+
+print(f'A média do aluno é: {media}')
