26ea6ff5a67f2a43fa04ba90ed33f9746b6ccabc
Stefany Beatriz <67984916+stefanybeatriz@users.noreply.github.com>
2024-03-27 09:18:21 -0300

    Add files via upload

---
+++ 016_quebrando_numeros.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira
+
+numero = float(input('Digite um número real qualquer: '))
+
+print(f'A poção inteira de {numero} equivale a {numero:.0f}')
