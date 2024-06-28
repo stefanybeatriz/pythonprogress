45a0c6c34357915c9f68c6d4469c3e568615f22d
Stefany Beatriz <67984916+stefanybeatriz@users.noreply.github.com>
2024-06-28 13:58:08 -0300

    Add files via upload

---
+++ 021_mp3.py
@@ -0,0 +1,6 @@
+# Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3
+
+from playsound import playsound
+
+playsound('C:/Users/tefab/PycharmProjects/pythonProject/venv/Scripts/inferno.mp3')
+
