98c87463f2cf59bccb2411315aedac574686d538
Stefany Beatriz <67984916+stefanybeatriz@users.noreply.github.com>
2024-07-01 16:50:44 -0300

    Add files via upload

---
+++ 030_par_ou_impar.py
@@ -0,0 +1,9 @@
+# Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+
+numero = int(input('Digite um valor: '))
+
+if numero % 2 == 0:
+    print('O valor digitado é um número par.')
+else:
+    print('O valor digitado é um número ímpar')
+
