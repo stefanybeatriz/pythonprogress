d4bc082696549662bd191d2e8df033f48b0bd9d0
Stefany Beatriz <67984916+stefanybeatriz@users.noreply.github.com>
2024-10-03 09:30:18 -0300

    Add files via upload

---
+++ 047_contagem_de_pares.py
@@ -0,0 +1,7 @@
+# Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50
+
+print('Os números pares que estão no intervalo entre 1 e 50 são: ')
+for c in range(1, 51):
+    if c % 2 == 0:
+        print(c)
+
